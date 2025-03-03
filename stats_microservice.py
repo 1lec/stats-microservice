0efd494b19f6f9600878ce801c7f212b15695f35
alec <alecandersen97@gmail.com>
2025-03-03 11:58:51 -0500

    Generates a leaderboard in JSON-valid format

--- stats_microservice.py
+++ stats_microservice.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 ZMQ_PORT = '5558'
 
+def decimal_to_percent(decimal):
+    """Receives a decimal representing a proportion and converts it to a percentage with up to 2 decimal places."""
+    return round(decimal * 100, 2)
 
 class DataAnalyzer:
     def __init__(self):
@@ -19,16 +22,24 @@ class DataAnalyzer:
     
     def calculate_winning_percentage(self, dataframe):
         """Receives a DataFrame of game results for a player, and, using the Results column, calculates the winning percentage of that player."""
-        decimal_value = dataframe["Results"].mean()  # from 0 to 1 with variable decimal places
-        win_percent = round(decimal_value * 100, 2)  # from 0.00 to 100.00 with up to 2 decimal places
+        decimal_value = dataframe["Results"].mean()      # from 0 to 1 with variable decimal places
+        win_percent = decimal_to_percent(decimal_value)  # from 0.00 to 100.00 with up to 2 decimal places
         self.socket.send_json({"win-percent": win_percent})
 
     def create_leaderboard(self, dataframe):
-        """Receives a Dataframe of game results for all players, and returns a list of the top 10 names by winning percentage in
-        descending order."""
-        series = dataframe.groupby("Name")[["Results"]].mean()
-        self.socket.send_string("Created leaderboard.")
-        print(series)
+        """Receives a Dataframe of game results for all players and sends a list of lists of name and winning percentage
+        sorted by winning percentage."""
+        # Groups all results by Name, calculates the winning percentage for each, and takes the top 10 in descending order
+        decimals = dataframe.groupby("Name")[["Results"]].mean()
+        sorted_decimals = decimals.sort_values(by="Results", ascending=False)
+        sorted_top_10_percents = sorted_decimals[["Results"]].apply(decimal_to_percent).head(10)
+
+        # Creates a JSON-safe list of lists from the above dataframe
+        top_10_json = []
+        for name, percent in sorted_top_10_percents.iterrows():
+            top_10_json.append([name, percent["Results"]])
+
+        self.socket.send_json({"leaderboard": top_10_json})
     
     def listen(self):
         """Listens for client requests."""
